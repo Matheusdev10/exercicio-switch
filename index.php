bb6c3aafc060a6fc026d1a9844c9285064428d33
Matheus Campos <matheus_501@hotmail.com>
2022-06-20 12:46:11 -0300

    exercicio switch php

---
+++ index.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Document</title>
+</head>
+<body>
+
+<div>
+<form method="get" action = "exercicio.php">
+Dia da semana : <input type= "number" name= "ds" min = "2" max = "8"  required = "8" />
+<input type= "submit" class= "botao" value ="Analisar"/>
+</form>
+
+</div>
+  
+</body>
+</html>
